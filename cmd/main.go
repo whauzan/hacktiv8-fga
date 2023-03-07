01df2765c35e4f0e625874b728a1f1927356ec97
whauzan <wahyu172845@gmail.com>
2023-03-08 01:22:12 +0700

    chapter 1 - challenge 1

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello! Wahyu here ^-^")
+	fmt.Printf("\n\nHello! Wahyu here ^-^ \n\n")
 
 	var i int
 	var j bool
@@ -11,23 +11,44 @@ func main() {
 
 	i = 21
 
-	fmt.Printf("%v \n", i)
-	fmt.Printf("%T \n", i)
-	fmt.Printf("%% \n")
+	fmt.Println("Menampilakan nilai i : 21")
+	fmt.Printf("%v \n\n", i)
+
+	fmt.Println("Menampilkan tipe data dari variabel i")
+	fmt.Printf("%T \n\n", i)
+
+	fmt.Println("Menampilkan tanda %")
+	fmt.Printf("%% \n\n")
 
 	j = true
-
-	fmt.Printf("%t \n", j)
+	fmt.Println("Menampilkan nilai boolean j : true")
+	fmt.Printf("%t \n\n", j)
 
 	j = false
+	fmt.Println("Menampilkan nilai boolean j : false")
+	fmt.Printf("%t \n\n", j)
 
-	fmt.Printf("%t \n", j)
-	fmt.Printf("%+q \n", "Я")
-	fmt.Printf("%d \n", 21)
-	fmt.Printf("%o \n", 25)
-	fmt.Printf("%x \n", "f")
-	fmt.Printf("%X \n", "F")
-	fmt.Printf("\u042F \n")
-	fmt.Printf("%f \n", k)
+	fmt.Println("Menampilkan unicode russia : Я (ya)")
+	fmt.Printf("%+q \n\n", "Я")
+
+	fmt.Println("Menampilkan nilai base 10 : 21")
+	fmt.Printf("%d \n\n", 21)
+
+	fmt.Println("Menampilkan nilai base 8 : 25")
+	fmt.Printf("%o \n\n", 25)
+
+	fmt.Println("Menampilkan nilai base 16 : f")
+	fmt.Printf("%x \n\n", "f")
+
+	fmt.Println("Menampilkan nilai base 16 : F")
+	fmt.Printf("%X \n\n", "F")
+
+	fmt.Println("Menampilkan unicode karakter Я : U+042F")
+	fmt.Printf("\u042F \n\n")
+
+	fmt.Println("Menampilkan float : 123.456000")
+	fmt.Printf("%f \n\n", k)
+
+	fmt.Println("Menampilkan float scientific : 1.234560E+02")
 	fmt.Printf("%E \n", k)
 }
