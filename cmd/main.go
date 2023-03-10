f413f583065378be5d8cabc09992bce8f9e80aea
whauzan <wahyu172845@gmail.com>
2023-03-11 05:09:23 +0700

    chapter 1 - challenge 2

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello! Wahyu here ^-^")
+	fmt.Printf("\n\nHello! Wahyu here ^-^\n\n")
+
+	charArr := []int{0x421, 0x51, 0x410, 0x51, 0x428, 0x51, 0x410, 0x51, 0x420, 0x51, 0x412, 0x51, 0x41e}
+
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Nilai i = %d\n", i)
+	}
+
+	for j := 0; j < 11; j++ {
+		if (j > 4 && j < 6) {
+			for i, char := range charArr {
+				if i % 2 == 0 {
+					fmt.Printf("character %U %q starts at byte position %d\n", char, char, i)
+				}
+			}
+			continue
+		}
+		fmt.Printf("Nilai j = %d\n", j)
+	}
 }
