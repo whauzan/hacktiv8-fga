af44d0777c12494ffe5621bb38797624925e1b87
whauzan <wahyu172845@gmail.com>
2023-03-12 13:47:04 +0700

    chapter 1 - challenge 3

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello! Wahyu here ^-^")
+	fmt.Printf("\n\nHello! Wahyu here ^-^\n\n")
+
+	text := "selamat malam"
+    counter := make(map[string]int)
+    for _, c := range text {
+        fmt.Printf("%v\n", string(c))
+        counter[string(c)]++
+    }
+
+    chars := make([]string, 0, len(counter))
+    for chr := range counter {
+        chars = append(chars, chr)
+    }
+
+	fmt.Println(counter)
 }
